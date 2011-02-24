19e5e7c368668bb45b897c0f45d8a8d3118c0ed0
Mike Frawley <frawl021@gmail.com>
2011-02-24 13:50:16 -0600

    simpler using_gmaps_js?

--- lib/bhm/google_maps/helper.rb
+++ lib/bhm/google_maps/helper.rb
@@ -3,17 +3,17 @@ module BHM
     module Helper
       
       def using_gmaps_js?
-        instance_variable_defined?(:@using_gmaps_js) && !!@using_gmaps_js
+        @using_gmaps_js
       end
-      
-      def google_maps_url(sensor = false)
-        "http://maps.google.com/maps/api/js?sensor=#{sensor}"
-      end
-      
+
       def use_gmaps_js
         return if using_gmaps_js?
         BHM::GoogleMaps.include_js_proc.call(self)
         @using_gmaps_js = true
+      end
+      
+      def google_maps_url(sensor = false)
+        "http://maps.google.com/maps/api/js?sensor=#{sensor}"
       end
       
       def draw_map_of(address, options = {})
